af6b4493eb0d5e9e070076c368c743882e4667fb
Daria Bystrova <dbystrova26@gmail.com>
2026-06-15 16:50:11 +0200

    fix: set async_mode=gevent to match gunicorn gevent worker

--- mvp/web/server.py
+++ mvp/web/server.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET", "insurvoice-dev-change-in-prod")
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
 
 ANTHROPIC_KEY  = os.getenv("ANTHROPIC_API_KEY", "")
 OPENAI_KEY     = os.getenv("OPENAI_API_KEY", "")
